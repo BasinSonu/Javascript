e0d54dfb779cf9b038442c2be6795c9aa0d5f1ee
Basin Sonu <basinsonu5@gmail.com>
2022-12-24 22:47:04 +0530

    may18 class notes and problems

---
+++ Arrays and Objects/Arrays_and_Objects-may18.js
@@ -0,0 +1,59 @@
+
+/* ********************************************  Print Minumum and Maximum  *************************************************/
+
+// function minandmax(arr){
+// for(j=0;j<arr.length;j++){
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]>arr[i+1]){
+//         temp=arr[i];
+//         arr[i]=arr[i+1];
+//         arr[i+1]=temp;
+//     }
+// }
+// }
+// console.log(arr);
+// console.log("The biggest number is "+arr[arr.length-1]);
+// console.log("The smallest number is "+arr[0]);
+// }
+// var arr=[2,5,200,1,9,6,100,43];
+// minandmax(arr);
+
+
+/*************************************************************  Maximum Profit *****************************************************/
+
+// function maximumprofit(arr){
+// var maxprofit=0;
+// for(let i=1;i<arr.length;i++){
+//     if(arr[i]>arr[i-1]){
+//         maxprofit=maxprofit+(arr[i]-arr[i-1]);
+//     }
+// }
+// console.log(maxprofit);
+// }
+// maximumprofit([10,12,8,20,12,20,24]);
+
+
+/************************************ Reverse the given Array ********************************************/
+// input = [1,3,5,2]
+
+
+// function reversearr(arr){
+// let arr1=[];
+// let j=0;
+// for(i=arr.length-1;i>=0;i--){
+//     arr1[j]=arr[i];
+//     j++;
+// }
+// console.log(arr1);
+// }
+
+// reversearr([1,3,5,2,10,5,7,11]);
+
+
+/*************** OR ******************/
+
+
+
+
+
+/************************************************************************ */
