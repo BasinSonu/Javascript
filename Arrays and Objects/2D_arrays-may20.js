a89c17a65dfdd2ff304ae6e149804174efe09ff3
Basin Sonu <basinsonu5@gmail.com>
2022-12-26 22:53:12 +0530

    may20 notes updated

--- Arrays and Objects/2D_arrays-may20.js
+++ Arrays and Objects/2D_arrays-may20.js
@@ -98,14 +98,14 @@
 
 let startingrow=0, startingcol=0, totalrow=5, totalcol=5;
 let arr=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]];
-while(startingrow<totalrow && startingcol<totalcol){
+// while(startingrow<totalrow && startingcol<totalcol){
+
+
+if(startingcol<totalcol){
     for(i=0;i<totalrow;i++){
         console.log(arr[i][startingcol]);
-    }
-    startingcol++;
-    if(i==5){
-        for(i=totalrow-1;i>=0;i--){
-            console.log(arr[i][startingcol]);
-        }
-    }
-}
+        startingrow++;
+    }startingcol++;
+}console.log(startingcol);
+
+
