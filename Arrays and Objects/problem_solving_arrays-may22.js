5b7273acbd6daaeba1b570c6f56b1a19a331ab50
Basin Sonu <basinsonu5@gmail.com>
2022-12-28 15:17:37 +0530

    may22 notes

---
+++ Arrays and Objects/problem_solving_arrays-may22.js
@@ -0,0 +1,32 @@
+
+/******************************************** Sorting **************************************/
+
+// let arr=[5,9,12,3,2,1,10];                              /***** 1st method***** */
+// let count=0;
+// while(count<arr.length){
+//     for(i=0;i<arr.length;i++){
+//         if(arr[i]>arr[i+1]){
+//         temp=arr[i];
+//         arr[i]=arr[i+1];
+//         arr[i+1]=temp;
+//         }
+//     }
+//     count++
+// }console.log(arr);
+
+/*******************************************************************************************************************************/
+/******** 2nd method *************/
+
+let arr=[5,9,12,3,2,1,10];
+console.log(arr);
+for(i=0;i<arr.length;i++){
+    for(j=0;j<arr.length;j++){
+        if(arr[j]>arr[j+1]){
+            temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}console.log(arr);
+
+*
