4426403c25e8ae0a6f81dba31fe0868072ad991c
Basin Sonu <basinsonu5@gmail.com>
2022-12-29 14:59:36 +0530

    may25 notes

---
+++ may25.js
@@ -0,0 +1,46 @@
+/****************************** Check eligibility to vote ******************************/
+
+// function checkeligibility(age){
+//     if(age>=18){
+//         console.log("Eligible to vote");
+//     }else if(age<18){
+//         console.log("Not eligible to vote");
+//     }else{
+//         console.log("Invalid statement");
+//     }
+// }
+// checkeligibility(20);
+
+
+/**************************** Percentage of attendence ********************************/
+
+// function checkattendancepercentage(attendendClass, totalClass){
+//     let attendence=(attendendClass/totalClass)*100;
+//     if(attendence<75){
+//         console.log("Not eligible to write exam");
+//     }else{
+//         console.log("Eligible to write exam");
+//     }
+// }
+// checkattendancepercentage(12,20);
+// checkattendancepercentage(80,100);
+
+/************************ Check order ***************************************/
+
+// function checkOrder(a,b,c){
+//     if(c>b && b>a){
+//         console.log("Increasing");
+//     }else if(a>b && b>c){
+//         console.log("Decreasing")
+//     }else{
+//         console.log("Nor increasing Nor decreasing");
+//     }
+// }
+// checkOrder(1,2,3);
+// checkOrder(5,3,1);
+// checkOrder(10,5,12);
+
+/************************************* ***************************************/
+
+
+let arr=[];
